a880a0dc5b1b99921e568a0f1693a700c8912d89
Alexey Nechaev <alexey.nechaev1988@gmail.com>
2020-09-22 17:53:01 +0300

    4-module

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -3,6 +3,13 @@
  * @param {Object[]} friends
  * @return {HTMLUListElement}
  */
+
 function makeFriendsList(friends) {
   // ваш код...
+  const ul = document.createElement('ul')
+
+  ul.innerHTML = friends.map( friend => '<li>' + friend.firstName + ' ' + friend.lastName + '</li>')
+  .join('')
+  
+  return ul
 }
